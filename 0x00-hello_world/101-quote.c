4f6e93019e7f91316e422af790d7c177a05d65d7
Sisanda <csandazondi01@gmail.com>
2023-07-17 13:56:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
  * main - Entry point
  *
  * Description: Print a new line to the standard error
- * Return 1 (Success)
+ * Return: Always 1 (Success)
 */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
